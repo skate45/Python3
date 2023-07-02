5e64de13b3eb23f0b8220c7281f716b797b9a9d2
Ignacio Castillo <ignacioce3@gmail.com>
2023-07-01 22:42:48 -0600

    Update FuncionesBasicas.py

--- FuncionesBasicas.py
+++ FuncionesBasicas.py
@@ -12,3 +12,23 @@ def solution(picture):
     
     return resultado
 
+#Boolean function if swaping one element in a at most equals b
+def solution(a, b):
+    position1=-1
+    position2=-1
+    diferentes=0
+    for i in range(len(a)):
+        if a[i]!=b[i]:
+            diferentes+=1
+            if position1==-1:
+                position1=i
+            else:
+                if position2==-1:
+                    position2=i
+                else:
+                    break
+    if diferentes==2:
+        return a[position1]==b[position2] and a[position2]==b[position1]
+    if diferentes==0:
+        return True
+    return False
